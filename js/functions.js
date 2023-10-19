ad014fc82de3d630aa987085676881ce44a5e525
egorReka <e.reka55@yandex.ru>
2023-10-20 00:30:19 +0300

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,22 @@ const reverseWord = (word) => {
 const isPalindrome = () => reverseWord(normalizeWord(WORD)) === normalizeWord(WORD);
 
 isPalindrome();
+
+// Делу — время
+const isWorkingHours = (start, end, meetings, length) => {
+  const startWorkDay = start.split(':');
+  const endWorkDay = end.split(':');
+  const startMeetings = meetings.split(':');
+  const lengthMeetings = length;
+
+  const timeLeft = ((parseInt(endWorkDay[0], 10) - parseInt(startMeetings[0], 10)) * 60) + (parseInt(endWorkDay[1], 10) + parseInt(startMeetings[1], 10));
+  const сheckStart = ((parseInt(startMeetings[0], 10) - parseInt(startWorkDay[0], 10)) * 60) + (parseInt(startWorkDay[1], 10)) + parseInt(startMeetings[1], 10);
+
+  return timeLeft >= lengthMeetings && сheckStart >= 0;
+};
+
+isWorkingHours('08:00', '17:30', '14:00', 90); // true
+isWorkingHours('8:0', '10:0', '8:0', 120); // true
+isWorkingHours('08:00', '14:30', '14:00', 90); // false
+isWorkingHours('14:00', '17:30', '08:0', 90); // false
+isWorkingHours('8:00', '17:30', '08:00', 900); // false
