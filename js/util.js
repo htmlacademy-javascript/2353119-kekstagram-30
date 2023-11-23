5a962226086299cba6de27c0dda265a6e2c1afdc
egorReka <e.reka55@yandex.ru>
2023-11-23 19:40:11 +0300

    module12-task1

--- js/util.js
+++ js/util.js
@@ -14,6 +14,41 @@ const showErrorMessage = () => {
   }, REMOVE_MESSAGE_TIMEOUT);
 };
 
+const getRandomIndex = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const getArrayUniqueIndexes = (min, max) => {
+  const arrayUniqueIndexes = [];
+
+  if (arrayUniqueIndexes.length >= (max - min + 1)) {
+    return null;
+  }
+
+  while (arrayUniqueIndexes.length < max) {
+    const randomIndex = getRandomIndex(min, max);
+
+    if (!arrayUniqueIndexes.includes(randomIndex)) {
+      arrayUniqueIndexes.push(randomIndex);
+    }
+  }
+
+  return arrayUniqueIndexes;
+};
+
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export { isEscapeKey, showErrorMessage };
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { isEscapeKey, showErrorMessage, getArrayUniqueIndexes, debounce };
